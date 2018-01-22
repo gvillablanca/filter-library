ed65999b092a53111d0c650ef2fd7027744a927b
gvillablanca <g.villalobo.m@gmail.com>
2018-01-22 17:45:20 -0300

    boton diseño y estilos desde javascript

--- src/app.js
+++ src/app.js
@@ -4,16 +4,22 @@ function grayScale(){
   const newContainer = document.createElement('div');
   const btnGrey = document.createElement('button');
   const greyText = document.createTextNode('GreyScale');
+  btnGrey.setAttribute("class", "btn btn-default");
   const btnSepia = document.createElement('button');
   const sepiaText = document.createTextNode('Sepia');
+  btnSepia.setAttribute("class", "btn btn-default");
   const btnHueRotate = document.createElement('button');
   const huerotateText = document.createTextNode('Hue rotate');
+  btnHueRotate.setAttribute("class", "btn btn-default");
   const btnInverse = document.createElement('button');
   const inverseText = document.createTextNode('Inverse');
+  btnInverse.setAttribute("class", "btn btn-default");
   const btnSaturate = document.createElement('button');
   const saturateText = document.createTextNode('Saturate');
+  btnSaturate.setAttribute("class", "btn btn-default");
   const btnNormal = document.createElement('button');
   const normalText = document.createTextNode('Normal');
+  btnNormal.setAttribute("class", "btn btn-default");   
 
   container.appendChild(imagen);
   newContainer.appendChild(btnGrey);
@@ -31,6 +37,24 @@ function grayScale(){
 
   const currentDiv = document.getElementById('div1');
   document.body.insertBefore(newContainer, currentDiv);
+
+  document.body.onload = styleElement;
+
+  function styleElement(){
+    newContainer.style.marginLeft = "25%";
+    newContainer.style.marginTop = "2%";
+    imagen.style.border = "1px solid #D8D8D8";
+    imagen.style.borderRadius = "5px";
+    imagen.style.marginLeft = "25%";
+    imagen.style.marginTop = "2%";
+    btnGrey.style.marginRight = "2%";
+    btnSepia.style.marginRight = "2%";
+    btnHueRotate.style.marginRight = "2%";
+    btnInverse.style.marginRight = "2%";
+    btnSaturate.style.marginRight = "2%";
+    btnNormal.style.marginRight = "2%";
+  }  
+
 
   btnGrey.addEventListener('click', ()=>{
     imagen.style.filter='grayscale(100%)';
